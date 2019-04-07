f2fc3e6f7c848e8e967f3ddc1ae2a1342fbc70b6
fangyin <806444032@qq.com>
2019-04-07 18:39:07 +0800

    初始版本

---
+++ src/main/java/com/example/boot/BootApplication.java
@@ -0,0 +1,29 @@
+package com.example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@EnableScheduling
+@RestController
+public class BootApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BootApplication.class);
+	}
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello, it's openapi service！";
+	}
+}
